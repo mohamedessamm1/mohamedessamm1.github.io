f6a4679f31f92973a606242d46662f4598881856
mohamedessamm1 <medomasr0@gmail.com>
2024-12-20 05:28:49 +0200

    second time

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1127067353"
+    serviceWorkerVersion: "1266015155"
   }
 });
